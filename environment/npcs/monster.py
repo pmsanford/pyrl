d06068310562f2312477870d45844fbead00035a
Paul Sanford <me@paulsanford.net>
2015-04-23 23:33:00 -0400

    monster docstrings

--- environment/npcs/monster.py
+++ environment/npcs/monster.py
@@ -1,19 +1,50 @@
 class Monster:
     def __init__(self, x, y, tile_code, hp = 100):
+        """
+        Create a new monster
+
+        :param int x: Initial location, x coord.
+        :param int y: Initial location, y coord.
+        :param str tile_code: Name of the tile to represent monster.
+        :param int hp: Initial health.
+        """
         self.tile_code = tile_code
         self.x = x
         self.y = y
         self.hp = hp
 
     def set_location(self, x, y):
+        """
+        Set the location of the monster.
+
+        :param int x: New location, x coord.
+        :param int y: New location, y coord.
+        """
         self.x = x
         self.y = y
 
     def get_location(self):
+        """
+        Get the location of the monster.
+
+        :return: Monster's location (x, y).
+        :rtype: tuple
+        """
         return (self.x, self.y)
 
     def take_damage(self, amount):
+        """
+        Deal damage to the monster.
+
+        :param int amount: Amount of damage dealt.
+        """
         self.hp -= amount
 
     def get_hp(self):
+        """
+        Get the monster's HP.
+
+        :return: Monster's current HP.
+        :rtype: int
+        """
         return self.hp
--- environment/npcs/monstercontroller.py
+++ environment/npcs/monstercontroller.py
@@ -1,6 +1,8 @@
 class MonsterController:
     def __init__(self, monster):
         """
+        Create a new monster controller.
+
         :param monster: The monster controlled by this controller.
         :type monster: environment.npcs.monster.Monster
         """
@@ -9,6 +11,11 @@ class MonsterController:
 
     def update(self, environment):
         """
+        Performs all the actions the monster needs to every frame. This
+        method iterates over the available behaviors. Once a behavior
+        notifies the method that it has acted, the method returns.
+
+
         :param environment: The environment in which the monster resides.
         :type environment: environment.environmentcontroller.EnvironmentController
         """
@@ -19,4 +26,9 @@ class MonsterController:
                 break
 
     def add_behavior(self, behavior):
+        """
+        Add a behavior to the list of possible behaviors for the monster.
+
+        :param behavior: The new behavior to add.
+        """
         self.behaviors.append(behavior)
